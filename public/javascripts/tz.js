eb7bca257212e7eae0feb232063eb50ddd11c5c9
augdog97 <augdog@me.com>
2021-01-28 03:37:35 -0500

    Finished Project: Added new current time text for adjusted clocks

--- public/javascripts/tz.js
+++ public/javascripts/tz.js
@@ -48,6 +48,7 @@ const nHour = nDate.getHours();
 const nMinute = nDate.getMinutes();
 const nSecond = nDate.getSeconds();
 
+
 // Creating a AM and PM conditional 
 // The conditional checks to see if the time is greater than or equal to 12 and if it is create a p element with a class of current and add current to the bottom of the local div and add the pm text to the bottom of the current p tag.
 // The text shows the current time with including the minutes and am or pm.
@@ -85,6 +86,9 @@ if (nHour >= 12) {
 } else {
     console.log("This is not a valid date");
 }
+
+
+
 
 
 // California Time //  ****************************
@@ -138,6 +142,8 @@ var nHr = moment.tz('America/Los_Angeles').hour();
 var nMin = moment.tz('America/Los_Angeles').minute();
 var nSec = moment.tz('America/Los_Angeles').second();
 
+
+
 // Creating a AM and PM conditional 
 // The conditional checks to see if the time is greater than or equal to 12 and if it is create a p element with a class of current and add current to the bottom of the local div and add the pm text to the bottom of the current p tag.
 // The text shows the current time with including the minutes and am or pm.
@@ -168,9 +174,11 @@ if (nHr >= 12) {
     currentDI.appendChild(currentPA);
     localC.appendChild(currentDI);
     currentPA.appendChild(amText);
-} else {
+}  else {
     console.log("This is not a valid date");
 }
+
+
 
 
 
@@ -317,6 +325,50 @@ $(document).ready(function () {
         secondHandN.style.transform = `rotate(${secPositionN}deg)`;
 
 
+        // Adjusted local time variables for am and pm formats
+        const dateNN = new Date();
+        window.milliAdd = interval * 60 * 60 * 1000;
+        dateNN.setMilliseconds(dateNN.getMilliseconds() + window.milliAdd);
+        const hourNN = dateNN.getHours();
+        const minuteNN = dateNN.getMinutes();
+
+        // Adjusted Time conditional to render text
+        if (hourNN >= 12) {
+            // Creating a DIV and Paragraph element and saving it to a variable
+            const currentD = document.createElement('DIV');
+            const currentP = document.createElement('P');
+            // Addding classes to the above created elements
+            currentD.className = ('currentD')
+            currentP.className = ('current');
+            //Saving the local class element to a variable
+            // Creating a text node using string interpolation to show the current time and PM
+            const local = document.querySelector('.local');
+            const pmText = document.createTextNode(`The current time is ${hourNN}:${minuteNN} P.M`);
+
+            // Adding the P element into the created Div element
+            // Then we are adding the created Div element into the .local element
+            // then We ar adding the current time text into the created paragraph element.
+            currentD.appendChild(currentP);
+            local.appendChild(currentD);
+            currentP.appendChild(pmText);
+        } else if (hourNN < 12) {
+
+            const currentD = document.createElement('DIV');
+            const currentP = document.createElement('P');
+            currentD.className = ('currentD')
+            currentP.className = ('current');
+
+            const local = document.querySelector('.local');
+            const amText = document.createTextNode(`The current time is ${hourNN}:${minuteNN} A.M`);
+
+            currentD.appendChild(currentP);
+            local.appendChild(currentD);
+            currentP.appendChild(amText);
+        } else {
+            console.log("This is not a valid date");
+        }
+
+
 
         // CALIFORNIA NEW TIME
 
@@ -352,12 +404,50 @@ $(document).ready(function () {
         let secPositionCN   = laSecN * 360 / 60;
 
 
+        
 
         // Applying the results from the above equation to the clock to show the correct hands position
         // only the variable names have changed.
         hourHandCN.style.transform = `rotate(${hrPositionCN}deg)`;
         minuteHandCN.style.transform = `rotate(${minPositionCN}deg)`;
         secondHandCN.style.transform = `rotate(${secPositionCN}deg)`;
+
+        // Variables for adjusted am pm 
+        var mN = moment().tz("America/Los_Angeles");
+        if (window.milliAdd2) {
+            mN = moment().add(window.milliAdd2, "ms").tz("America/Los_Angeles");
+        }
+        var nHrC = mN.hour();
+        var nMinC = mN.tz("America/Los_Angeles").minute();
+
+        if (nHrC >= 12) {
+            // Creating P element
+            const currentDI = document.createElement('DIV');
+            const currentPA = document.createElement('P');
+            currentDI.className = ('currentDI')
+            currentPA.className = ('current');
+            // Creating text node to show time.
+            const localC = document.querySelector('.california');
+            const pmText = document.createTextNode(`The current time is ${nHrC}:${nMinC} P.M`);
+            // appending to render time
+            currentDI.appendChild(currentPA);
+            localC.appendChild(currentDI);
+            currentPA.appendChild(pmText);
+        } else if (nHrC < 12) {
+
+            const currentDI = document.createElement('DIV');
+            const currentPA = document.createElement('P');
+            currentDI.className = ('currentDI')
+            currentPA.className = ('currentPA');
+
+            const localC = document.querySelector('.california');
+            const amText = document.createTextNode(`The current time is ${nHrC}:${nMinC} A.M`);
+            currentDI.appendChild(currentPA);
+            localC.appendChild(currentDI);
+            currentPA.appendChild(amText);
+        } else {
+            console.log("This is not a valid date");
+        }
 
 
 
@@ -403,6 +493,47 @@ $(document).ready(function () {
         hourHandLN.style.transform = `rotate(${hrPositionLN}deg)`;
         minuteHandLN.style.transform = `rotate(${minPositionLN}deg)`;
         secondHandLN.style.transform = `rotate(${secPositionLN}deg)`;
+
+
+        // Variables for adjusted am pm formats
+        var mM = moment().tz("Europe/London");
+        if (window.milliAdd3) {
+            mM = moment().add(window.milliAdd3, "ms").tz("Europe/London");
+        }
+        var loHrNN = mM.hour();
+        var loMinNN = mM.tz("Europe/London").minute();
+
+        // London Adjusted time Conditional to render am or pm formats
+        if (loHrNN >= 12) {
+            // Creating P element
+            const currentLD = document.createElement('DIV');
+            const currentLP = document.createElement('P');
+            currentLD.className = ('currentLD')
+            currentLP.className = ('currentLP');
+            // Creating text node to show time.
+            const localLo = document.querySelector('.london');
+            const pmText = document.createTextNode(`The current time is ${loHrNN}:${loMinNN} P.M`);
+            // appending to render time
+            currentLD.appendChild(currentLP);
+            localLo.appendChild(currentLD);
+            currentLP.appendChild(pmText)
+        } else if (loHrNN < 12) {
+
+            const currentLD = document.createElement('DIV');
+            const currentLP = document.createElement('P');
+            currentLD.className = ('currentLD')
+            currentLP.className = ('currentLP');
+
+
+            const localLo = document.querySelector('.london');
+            const amText = document.createTextNode(`The current time is ${loHrNN}:${loMinNN} A.M`);
+
+            currentLD.appendChild(currentLP);
+            localLo.appendChild(currentLD);
+            currentLP.appendChild(amText);
+        } else {
+            console.log("This is not a valid date");
+        }
 
 
         // Applying a value to the currentSetInterval to be the intervalInput value and passing that new value into each function. Thew new function only gets called whjen the button is clicked. 
